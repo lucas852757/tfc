11a94029b6bbb981a6945026ea1260226553438d
Lucas <bithome1@outlook.com>
2022-11-22 21:27:38 -0300

    criad app/backend/src/database/migrations/20221123000017-users.js

---
+++ app/backend/src/database/migrations/20221123000017-users.js
@@ -0,0 +1,45 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.createTable('users',{ id:{
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+     },
+     userName: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      fild: 'username',
+     },
+     role: {
+      type: Sequelize.STRING,
+      allowNull: false,
+     },
+     email: {
+      type: Sequelize.STRING,
+      allowNull: false
+     },
+     password: {
+      type: Sequelize.STRING,
+      allowNull: false
+     }
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.dropTable('users');
+  }
+};
