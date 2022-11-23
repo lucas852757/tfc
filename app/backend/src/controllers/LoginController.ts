b5161d65a50b48cec54c2598022c985b43dbd5bd
Lucas <bithome1@outlook.com>
2022-11-23 19:29:41 -0300

    feat: Retorna status 200 com resposta

--- app/backend/src/controllers/LoginController.ts
+++ app/backend/src/controllers/LoginController.ts
@@ -1,12 +1,16 @@
+import { Request, Response, NextFunction } from 'express';
 import LoginService from '../services/LoginService';
 
 class LoginController {
   constructor(private loginService = new LoginService()) {}
-  public login = () => {
+  public login = async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const response = this.loginService.login();
+      const { body } = req;
+      const response = await this.loginService.login(body);
+      return res.status(200).json(response);
     } catch (error) {
-      
+      console.log(error);
+      next(error);
     }
   };
 }
