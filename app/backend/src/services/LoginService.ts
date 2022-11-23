ebaaadb13c7aa28510aa7980724f56324ca745e4
Lucas <bithome1@outlook.com>
2022-11-23 19:31:10 -0300

    feat: Adicionado featuares para retorno de um token

--- app/backend/src/services/LoginService.ts
+++ app/backend/src/services/LoginService.ts
@@ -1,6 +1,32 @@
+import bcrypt = require('bcryptjs');
+import jsonwebtoken = require('jsonwebtoken');
+import Users from '../database/models/UserModel';
+
 class LoginService {
-  private _response: any;
-  public login = () => {};
+  private _response: Users | null | undefined;
+  public login = async (arg: { email: string; password: string; }) => {
+    const { email, password } = arg;
+    this._response = await Users.findOne({ where: { email } });
+    if (this._response === null) {
+      const error = new Error('Incorrect email or password');
+      error.name = 'UnauthorizedUserError';
+      throw error;
+    }
+
+    const { password: passwordIntoDB } = this._response?.toJSON() as Users;
+
+    const response = await bcrypt.compare(password, passwordIntoDB);
+
+    if (!response) {
+      const error = new Error('Incorrect email or password');
+      error.name = 'UnauthorizedUserError';
+      throw error;
+    }
+
+    const secret = process.env.JWT_SECRET;
+    const token = jsonwebtoken.sign({ data: this._response }, secret as jsonwebtoken.Secret);
+    return { token };
+  };
 }
 
 export default LoginService;
