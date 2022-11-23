e8fc34e7f81504a9b3d0319d98b4aca299e32b6a
Lucas <bithome1@outlook.com>
2022-11-23 15:41:26 -0300

    feat: Criado pasta routes

--- app/backend/src/controllers/LoginController.ts
+++ app/backend/src/controllers/LoginController.ts
@@ -1,5 +1,14 @@
-class LoginController {
+import LoginService from '../services/LoginService';
 
+class LoginController {
+  constructor(private loginService = new LoginService()) {}
+  public login = () => {
+    try {
+      const response = this.loginService.login();
+    } catch (error) {
+      
+    }
+  };
 }
 
 export default LoginController;
