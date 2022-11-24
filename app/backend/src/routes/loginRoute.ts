afef5dd9d3aba15591852d091a2e2f0158c733bf
Lucas <bithome1@outlook.com>
2022-11-23 22:47:29 -0300

    feat: Adicionado  validação para email e password

--- app/backend/src/routes/loginRoute.ts
+++ app/backend/src/routes/loginRoute.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
+import validate from '../middlewares/validate';
+
 import LoginController from '../controllers/LoginController';
 
 const loginController = new LoginController();
 const route = Router();
 
-route.post('/', loginController.login);
+route.post('/', validate, loginController.login);
 
 export default route;
---
+++ app/backend/src/tests/login.test.ts
@@ -0,0 +1,110 @@
+// lecture24.4
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+
+import App from '../app';
+const { app } = new App();
+// import Example from '../database/models/ExampleModel';
+// import models from '../database/models';
+import { Response } from 'superagent';
+import Users from '../database/models/UserModel';
+import * as bcrypt from 'bcryptjs';
+
+chai.use(chaiHttp);
+
+
+describe('routes/login', () => {
+  beforeEach(sinon.restore);
+
+  describe('Post /login', () => {
+
+    it('Retorna 500 quando o banco retorna um erro', async () => {
+    sinon.stub(Users, 'findOne' ).rejects('bankError');
+    const response = await chai.request(app)
+    .post('/login')
+    .send({
+      email: 'fake@admin.com',
+      password: "secret_admin"
+});
+
+    chai.expect(response.status).to.be.equal(500);
+    });
+
+    it('Retorna 400 quando a requisição é mal formatada', async () => {
+      // sinon.stub(Users, 'findOne' ).resolves(null);
+    const response = await chai.request(app)
+    .post('/login')
+    .send({
+      password: "secret_admin"
+});
+
+    chai.expect(response.status).to.be.equal(400);
+    });
+
+    it('Retorna 401 quando usuário não é encontrado', async () => {
+      sinon.stub(Users, 'findOne').resolves(null);
+      const response = await chai.request(app)
+      .post('/login/')
+      .send({
+        email: "Edmin@admin.com",
+        password: "secret_admin"
+      });
+
+      chai.expect(response.body).to.be.deep.equal({
+        "message": "Incorrect email or password"
+      });
+      chai.expect(response.status).to.be.equal(401);
+    });
+
+     it('Retorna 401 quando falha a autenticação', async () => {
+      const hashPassword= bcrypt.hashSync('codedPassword', 10);
+      const object: any = {
+          dataValues: {
+            id: 1,
+            userName: 'fakeName',
+            role: 'fakeAdmin',
+            email: 'fake@admin.com',
+            password: hashPassword
+          },
+      }
+      sinon.stub(Users, 'findOne' ).resolves(object);
+      const response = await chai.request(app)
+      .post('/login')
+      .send({
+        email: "fake@admin.com",
+        password: "invalidPassword"
+      });
+
+      chai.expect(response.body).to.be.deep.equal({
+        "message": "Incorrect email or password"
+      });
+      chai.expect(response.status).to.be.equal(401);
+
+    });
+
+    it ('Retorna 200 quando o usuário é registrado com sucesso', async () => {
+    const hashPassword= bcrypt.hashSync('password1234', 10);
+    const object: any = {
+      dataValues: {
+        id: 1,
+        userName: 'fakeName',
+        role: 'fakeAdmin',
+        email: 'fake@admin.com',
+        password: hashPassword
+      },
+  }
+    sinon.stub(Users, 'findOne' ).resolves(object);
+    const response = await chai.request(app)
+    .post('/login')
+    .send({
+      email: 'fake@admin.com',
+      password: 'password1234'
+});
+    chai.expect(response.status).to.be.equal(200);
+    chai.expect(response.body).to.contain.keys('token');
+    });
+  });
+  
+});
