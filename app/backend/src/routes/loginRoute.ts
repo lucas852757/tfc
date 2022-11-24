2987a6f37799d61c6a95cd73f9a4daec2dddf8c6
Lucas <bithome1@outlook.com>
2022-11-24 19:28:55 -0300

    feat: Criado route.get('/validate', validate); em loginRoute.ts

--- app/backend/src/routes/loginRoute.ts
+++ app/backend/src/routes/loginRoute.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
-import validateEmailAndUser from '../middlewares/validateEmailAndUserts';
+import validateEmailAndUser from '../middlewares/validateEmailAndUser';
 
 import LoginController from '../controllers/LoginController';
+import validate from '../middlewares/validate';
 
 const loginController = new LoginController();
 const route = Router();
 
+route.get('/validate', validate);
 route.post('/', validateEmailAndUser, loginController.login);
 
 export default route;
