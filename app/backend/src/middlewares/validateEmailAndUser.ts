d42fc78bdf23db87d7f87e52ca786743d4bc680b
Lucas <bithome1@outlook.com>
2022-11-24 16:52:09 -0300

    feat: Criado função validateEmailAndUser.ts

---
+++ app/backend/src/middlewares/validateEmailAndUser.ts
@@ -0,0 +1,23 @@
+import { Request, Response, NextFunction } from 'express';
+import Joi = require('joi');
+
+const validate = (req: Request, _res: Response, next: NextFunction) => {
+  try {
+    const { email, password } = req.body;
+    const schema = Joi.object({
+      email: Joi.string().email().required()
+        .messages({ 'string.empty': 'All fields must be filled' }),
+      password: Joi.string().not().empty().required()
+        .messages({ 'string.empty': 'All fields must be filled' }),
+    });
+    const { error } = schema.validate({ email, password });
+    console.log(error);
+    if (error) {
+      throw error;
+    }
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
+export default validate;
