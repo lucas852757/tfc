8e8c9aece5053949b9e1315c6bbece5a15cbae42
Lucas <bithome1@outlook.com>
2022-11-25 12:56:32 -0300

    feat: Criado migration teams

---
+++ app/backend/src/database/migrations/20221125154638-teams.js
@@ -0,0 +1,34 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.createTable('teams',{
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey:true,
+      },
+      teamName: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        field: 'team_name',
+      }
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+     await queryInterface.dropTable('teams');
+  }
+};
