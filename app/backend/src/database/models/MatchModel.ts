4e070030af3e806b393a4db897fcc4aaf202c536
Lucas <bithome1@outlook.com>
2022-11-26 18:03:38 -0300

    feat: Criado arquivo MatchModel.ts

---
+++ app/backend/src/database/models/MatchModel.ts
@@ -0,0 +1,42 @@
+import { INTEGER, Model } from 'sequelize';
+import db from '.';
+
+class Match extends Model {
+  id!: number;
+  homeTeam!: number;
+  homeTeamGoals!: number;
+  awayTeam!: number;
+  awayTeamGoals!: number;
+  inProgress!: number | boolean;
+}
+
+Match.init({
+  id: {
+    type: INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  homeTeamGoals: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  awayTeam: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  awayTeamGoals: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  inProgress: {
+    type: INTEGER,
+    allowNull: false,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  tableName: 'matches',
+  modelName: 'Match',
+});
+
+export default Match;
