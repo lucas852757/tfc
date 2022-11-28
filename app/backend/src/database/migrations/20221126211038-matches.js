47ff08dee6a054b2c56c4e633ef5738a5c3a11f5
Lucas <bithome1@outlook.com>
2022-11-28 19:46:16 -0300

    perf: No type de inProgress, foi substituído o Sequelize.INTERGER por Sequelize.BOOLEAN

--- app/backend/src/database/migrations/20221126211038-matches.js
+++ app/backend/src/database/migrations/20221126211038-matches.js
@@ -49,7 +49,7 @@ module.exports = {
       field: 'away_team_goals',
     },
     inProgress: {
-      type: Sequelize.INTEGER,
+      type: Sequelize.BOOLEAN,
       allowNull: false,
       field: 'in_progress',
     }
