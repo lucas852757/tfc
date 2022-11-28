18579a5b9158a3580bd97a0c76b3e6900e03aa3e
Lucas <bithome1@outlook.com>
2022-11-28 20:00:47 -0300

    feat: Criado arquivo MatchService.ts

---
+++ app/backend/src/services/MatchService.ts
@@ -0,0 +1,19 @@
+import Team from '../database/models/TeamModel';
+import Match from '../database/models/MatchModel';
+
+class MatchService {
+  private _response: Match[] = [];
+
+  public match = async () => {
+    this._response = await Match.findAll({
+      include: [
+        { model: Team, as: 'teamHome', attributes: { exclude: ['id'] } },
+        { model: Team, as: 'teamAway', attributes: { exclude: ['id'] } },
+      ],
+    });
+    
+    return this._response;
+  };
+}
+
+export default MatchService;
