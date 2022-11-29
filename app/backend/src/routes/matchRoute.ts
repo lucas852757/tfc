37bbc2835feea0125f384d044737f13de87f8fbe
Lucas <bithome1@outlook.com>
2022-11-29 17:16:22 -0300

    feat: Adicionado validador de token na rota de post /matches

--- app/backend/src/routes/matchRoute.ts
+++ app/backend/src/routes/matchRoute.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import MatchController from '../controllers/MatchController';
 import MatchPostController from '../controllers/MatchPostController';
+import validateToken from '../middlewares/validateToken';
 
 const matchController = new MatchController();
 const matchPostController = new MatchPostController();
 const route = Router();
 
-route.post('/', matchPostController.matchPost);
+route.post('/', validateToken, matchPostController.matchPost);
 route.get('/', matchController.match);
 
 export default route;
