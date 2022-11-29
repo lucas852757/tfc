fac1f3cd70481de0c6b6023c5aa8db474176d7c8
Lucas <bithome1@outlook.com>
2022-11-29 16:30:01 -0300

    feat: Criado rota de post  para /matches

--- app/backend/src/routes/matchRoute.ts
+++ app/backend/src/routes/matchRoute.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
 import MatchController from '../controllers/MatchController';
+import MatchPostController from '../controllers/MatchPostController';
 
 const matchController = new MatchController();
+const matchPostController = new MatchPostController();
 const route = Router();
 
+route.post('/', matchPostController.matchPost);
 route.get('/', matchController.match);
 
 export default route;
