106a2ac3125aadabcbae6799fb5c3b68cbd3252e
Lucas <bithome1@outlook.com>
2022-11-30 17:48:28 -0300

    fix: Renomeado atributo _response para response e tornado-o público

--- app/backend/src/services/MatchService.ts
+++ app/backend/src/services/MatchService.ts
@@ -3,30 +3,30 @@ import Match from '../database/models/MatchModel';
 import filter from '../solid/choiceTrueOrFalse';
 
 class MatchService {
-  private _response: Match[] = [];
+  public response: Match[] = [];
 
   public match = async (arg: string) => {
-    this._response = await Match.findAll({
+    this.response = await Match.findAll({
       include: [
         { model: Team, as: 'teamHome', attributes: { exclude: ['id'] } },
         { model: Team, as: 'teamAway', attributes: { exclude: ['id'] } },
       ],
     });
 
-    if (!this._response.length) {
+    if (!this.response.length) {
       const error = new Error('Not found error');
       error.name = 'notFoundError';
       throw error;
     }
 
     if (arg === 'true') {
-      return filter(arg, this._response);
+      return filter(arg, this.response);
     }
     if (arg === 'false') {
-      return filter(arg, this._response);
+      return filter(arg, this.response);
     }
 
-    return this._response;
+    return this.response;
   };
 }
 
