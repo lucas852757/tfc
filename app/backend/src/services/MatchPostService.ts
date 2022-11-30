a4c904db2af77fecbd866dde9c8011495cc27dc1
Lucas <bithome1@outlook.com>
2022-11-30 17:02:06 -0300

    feat:

--- app/backend/src/services/MatchPostService.ts
+++ app/backend/src/services/MatchPostService.ts
@@ -1,7 +1,11 @@
+import teamsNotEqual from '../solid/teamsNotEqual';
 import Match from '../database/models/MatchModel';
+import checkouId from '../solid/checkoutId';
+import Team from '../database/models/TeamModel';
 
 class MatchPostService {
   public _response!: Match;
+  private _homeTeamAndAwayTeam!: Team[];
 
   public matchPost = async (
     homeTeam: number,
@@ -12,6 +16,18 @@ class MatchPostService {
     const obj = {
       homeTeam, awayTeam, homeTeamGoals, awayTeamGoals, inProgress: true,
     };
+
+    teamsNotEqual(homeTeam, awayTeam);
+    // Attention here
+    this._homeTeamAndAwayTeam = await checkouId(homeTeam, awayTeam);
+
+    if (this._homeTeamAndAwayTeam.length < 2) {
+      const error = new Error('There is no team with such id!');
+      error.name = 'notFoundError';
+      throw error;
+    }
+    // If the teams are not equal, it's allright
+
     this._response = await Match.create(obj);
     return this._response;
   };
