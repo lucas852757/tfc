4d40a035910f7517c9b8c0942720deac0d49daa0
Lucas <bithome1@outlook.com>
2022-11-30 11:06:35 -0300

    feat: Criado rota /matches/:id/finish

--- app/backend/src/routes/matchRoute.ts
+++ app/backend/src/routes/matchRoute.ts
@@ -1,13 +1,16 @@
 import { Router } from 'express';
 import MatchController from '../controllers/MatchController';
 import MatchPostController from '../controllers/MatchPostController';
+import MatchesPatchIdController from '../controllers/MatchesPatchIdFinishController';
 import validateToken from '../middlewares/validateToken';
 
 const matchController = new MatchController();
 const matchPostController = new MatchPostController();
+const matchPatchIdController = new MatchesPatchIdController();
 const route = Router();
 
 route.post('/', validateToken, matchPostController.matchPost);
 route.get('/', matchController.match);
+route.patch('/:id/finish', matchPatchIdController.matchesPatchId);
 
 export default route;
