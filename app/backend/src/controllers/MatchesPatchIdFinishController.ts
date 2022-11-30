1c1a9f19677206852406c5444a715cf987330be4
Lucas <bithome1@outlook.com>
2022-11-30 11:07:42 -0300

    feat: Criado arquivo MatchesPatchIdFinishController.ts

---
+++ app/backend/src/controllers/MatchesPatchIdFinishController.ts
@@ -0,0 +1,20 @@
+import { Request, Response, NextFunction } from 'express';
+
+import MatchesPatchIdFinish from '../services/MatchesPatchIdFinishService';
+
+class MatchesPatchIdController {
+  constructor(private matchesPatchIdFinish = new MatchesPatchIdFinish()) {}
+
+  public matchesPatchId = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { id } = req.params;
+      const response = await this.matchesPatchIdFinish.matchesIdFinish(parseInt(id, 10));
+      return res.status(200).json(response);
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
+  };
+}
+
+export default MatchesPatchIdController;
