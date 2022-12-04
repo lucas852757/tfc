3328b73501e590804fb5a99cf22d2910f60ec62e
Lucas <bithome1@outlook.com>
2022-12-03 23:34:03 -0300

    feat: Adicionado função para ordenar resposta para o endpoint  Get /leaderboard/home

--- app/backend/src/services/LeaderBoardHomeService.ts
+++ app/backend/src/services/LeaderBoardHomeService.ts
@@ -1,5 +1,4 @@
-/*
-source:https://app.betrybe.com/learn/course/5e938f69-6e32-43b3-9685-c936530fd326/module/94d0e996-1827-4fbc-bc24-c99fb592925b/section/0ca77b1d-4770-4646-8368-167d2305e763/day/94e113d7-6a86-4536-a1d3-08f55f557811/lesson/cf69bd1a-0095-44f0-be02-7cd9ddad0ac2 */
+/* source:https://app.betrybe.com/learn/course/5e938f69-6e32-43b3-9685-c936530fd326/module/94d0e996-1827-4fbc-bc24-c99fb592925b/section/0ca77b1d-4770-4646-8368-167d2305e763/day/94e113d7-6a86-4536-a1d3-08f55f557811/lesson/cf69bd1a-0095-44f0-be02-7cd9ddad0ac2 */
 
 import Team from '../database/models/TeamModel';
 import Match from '../database/models/MatchModel';
@@ -11,6 +10,8 @@ import totalLosses from '../solidHelpers.ts/totalLosses';
 import totalGoalsFavor from '../solidHelpers.ts/totalGoalsFavor';
 import totalGoalsOwn from '../solidHelpers.ts/totalGoalsOwn';
 import efficiency from '../solidHelpers.ts/efficiency';
+import sortArray from '../solid/sortArray';
+import Rank from '../interfaces/RankInterface';
 
 class LeaderBoardHomeService {
   private _response!: Team[];
@@ -20,7 +21,7 @@ class LeaderBoardHomeService {
       include: { model: Match, as: 'teamHome', where: { inProgress: false } },
     });
 
-    return this._response.map((o) => ({
+    this._response.map((o) => ({
       name: o.teamName,
       totalPoints: totalPoints(o.teamHome as unknown as Match[]),
       totalGames: totalGames(o.teamHome as unknown as Match[]),
@@ -29,10 +30,12 @@ class LeaderBoardHomeService {
       totalLosses: totalLosses(o.teamHome as unknown as Match[]),
       goalsFavor: totalGoalsFavor(o.teamHome as unknown as Match[]),
       goalsOwn: totalGoalsOwn(o.teamHome as unknown as Match[]),
-      goalsBalence: totalGoalsFavor(o.teamHome as unknown as Match[])
-      - totalGoalsOwn(o.teamHome as unknown as Match[]),
+      goalsBalence:
+          totalGoalsFavor(o.teamHome as unknown as Match[])
+          - totalGoalsOwn(o.teamHome as unknown as Match[]),
       efficiency: efficiency(o.id, o.teamHome as unknown as Match[]),
     }));
+    return sortArray(this._response as unknown as Rank[]);
   };
 }
 
