e545f7f69263531be91d9c5c161c9602c18647a5
Lucas <bithome1@outlook.com>
2022-12-03 23:31:00 -0300

    feat: Criado arquivo RankInterface.ts

---
+++ app/backend/src/interfaces/RankInterface.ts
@@ -0,0 +1,14 @@
+interface Rank {
+  name:string,
+  totalPoints: number,
+  totalGames: number,
+  totalVictories: number,
+  totalDraws: number,
+  totalLosses: number,
+  goalsFavor: number,
+  goalsOwn: number,
+  goalsBalence: number,
+  efficiency: string,
+}
+
+export default Rank;
