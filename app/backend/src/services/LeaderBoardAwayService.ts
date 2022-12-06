6369b50cf8856be338a91f9a37f2c577360baae8
Lucas <bithome1@outlook.com>
2022-12-05 23:04:02 -0300

    feat: Criado arquivo LeaderBoardAwayService.ts

---
+++ app/backend/src/services/LeaderBoardAwayService.ts
@@ -0,0 +1,28 @@
+import Match from "../database/models/MatchModel";
+import Team from "../database/models/TeamModel";
+import calcLeaderBoardAway from "../solid/calcLeaderBoard";
+
+class LeaderBoardAwayServices {
+  private _response!: Team[];
+
+  public leaderBoard = async () => {
+    this._response = await Team.findAll({
+      include: { model: Match, as: "awayTeam", where: { inProgress: false } },
+    });
+
+    return calcLeaderBoardAway(this._response).sort((a, b) => {
+      if (a.totalPoints === b.totalPoints) {
+        if (a.goalsBalance === b.goalsBalance) {
+          if (a.goalsFavor === b.goalsFavor) {
+            return a.goalsOwn - b.goalsOwn;
+          }
+          return b.goalsFavor - a.goalsFavor;
+        }
+        return b.goalsBalance - a.goalsBalance;
+      }
+      return b.totalPoints - a.totalPoints;
+    });
+  };
+}
+
+export default LeaderBoardAwayServices;
