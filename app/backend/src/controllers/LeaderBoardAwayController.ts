b014c8a453e2791cd0ed4ab2f46d501a78435f39
Lucas <bithome1@outlook.com>
2022-12-05 23:07:50 -0300

    feat: Criado arquivo LeaderBoardAwayController.ts

---
+++ app/backend/src/controllers/LeaderBoardAwayController.ts
@@ -0,0 +1,17 @@
+import { Request, Response, NextFunction } from 'express';
+import LeaderBoardAwayService from "../services/LeaderBoardAwayService";
+ 
+class LeaderBoardAwayController {
+  constructor(private leaderBoardAwayService = new LeaderBoardAwayService()){}
+
+  public leaderBoardAway = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const response = await this.leaderBoardAwayService.leaderBoard();
+      return res.status(200).json(response);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
+
+export default LeaderBoardAwayController; 
