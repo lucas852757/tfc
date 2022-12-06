189a5c4b3b1970bbf856e0c0aaed8b2f57f602e4
Lucas <bithome1@outlook.com>
2022-12-05 23:08:47 -0300

    feat: Adicionado rota /away

--- app/backend/src/routes/leaderBoardHomeRoute.ts
+++ app/backend/src/routes/leaderBoardHomeRoute.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
 import LeaderBoardHomeController from '../controllers/LeaderBoardHomeController';
+import LeaderBoardAwayController from '../controllers/LeaderBoardAwayController';
 
 const leaderBoardHomeController = new LeaderBoardHomeController();
+const leaderBoardAwayController = new LeaderBoardAwayController();
 const route = Router();
 
 route.get('/home', leaderBoardHomeController.leaderBoardHome);
+route.get('/away', leaderBoardAwayController.leaderBoardAway);
 
 export default route;
