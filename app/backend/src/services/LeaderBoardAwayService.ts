3a94b0d6d4b3495374f200fd525d0f1e22b2214a
Lucas <bithome1@outlook.com>
2022-12-09 13:52:38 -0300

    fix: Corrigido bug no eager load no arquivo LeaderBoardAwayService.ts

--- app/backend/src/services/LeaderBoardAwayService.ts
+++ app/backend/src/services/LeaderBoardAwayService.ts
@@ -1,16 +1,16 @@
 import Match from "../database/models/MatchModel";
 import Team from "../database/models/TeamModel";
-import calcLeaderBoardAway from "../solid/calcLeaderBoard";
+import calcLeaderBoardAway from "../solid/calcLeaderBoardHome";
 
 class LeaderBoardAwayServices {
   private _response!: Team[];
 
   public leaderBoard = async () => {
     this._response = await Team.findAll({
-      include: { model: Match, as: "awayTeam", where: { inProgress: false } },
+      include: { model: Match, as: "teamAway", where: { inProgress: false } },
     });
 
-    return calcLeaderBoardAway(this._response).sort((a, b) => {
+    /* return calcLeaderBoardAway(this._response).sort((a, b) => {
       if (a.totalPoints === b.totalPoints) {
         if (a.goalsBalance === b.goalsBalance) {
           if (a.goalsFavor === b.goalsFavor) {
@@ -21,7 +21,8 @@ class LeaderBoardAwayServices {
         return b.goalsBalance - a.goalsBalance;
       }
       return b.totalPoints - a.totalPoints;
-    });
+    }); */
+    return this._response;
   };
 }
 
