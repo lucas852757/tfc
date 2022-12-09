69e542c2ce427756658a940c4d87376f082a7323
Lucas <bithome1@outlook.com>
2022-12-09 20:08:56 -0300

    feat: Criado arquivo totalLosses.ts para awayTeam

---
+++ app/backend/src/solidLeaderBoardHome/totalLosses.ts
@@ -0,0 +1,15 @@
+import Match from '../database/models/MatchModel';
+
+// This function calculate the number of losses of each team
+const totalLosses = (list: Match[]) => {
+  let losses = 0;
+
+  for (let count = 0; count < list.length; count += 1) {
+    if (list[count].awayTeamGoals < list[count].homeTeamGoals) {
+      losses += 1;
+    }
+  }
+  return losses;
+};
+
+export default totalLosses;
