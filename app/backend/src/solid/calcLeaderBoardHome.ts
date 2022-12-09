ff5a0cc7c0f48b2ed1a84df6d91f8018581a9d8e
Lucas <bithome1@outlook.com>
2022-12-09 14:07:09 -0300

    feat: Criado arquivo calcLeaderBoardHome.ts

---
+++ app/backend/src/solid/calcLeaderBoardHome.ts
@@ -0,0 +1,29 @@
+import Match from '../database/models/MatchModel';
+import efficiency from '../solidHelpers.ts/efficiency';
+import totalDraws from '../solidHelpers.ts/totalDraws';
+import totalGames from '../solidHelpers.ts/totalGames';
+import totalGoalsFavor from '../solidHelpers.ts/totalGoalsFavor';
+import totalGoalsOwn from '../solidHelpers.ts/totalGoalsOwn';
+import totalLosses from '../solidHelpers.ts/totalLosses';
+import totalPoints from '../solidHelpers.ts/totalPoints';
+import totalVictories from '../solidHelpers.ts/totalVictories';
+import Team from '../database/models/TeamModel';
+// import Rank from '../interfaces/RankInterface';
+
+const calcLeaderBoardHome = (list: Team[]) =>
+  list.map((o) => ({
+    name: o.teamName,
+    totalPoints: totalPoints(o.teamHome as unknown as Match[]),
+    totalGames: totalGames(o.teamHome as unknown as Match[]),
+    totalVictories: totalVictories(o.teamHome as unknown as Match[]),
+    totalDraws: totalDraws(o.teamHome as unknown as Match[]),
+    totalLosses: totalLosses(o.teamHome as unknown as Match[]),
+    goalsFavor: totalGoalsFavor(o.teamHome as unknown as Match[]),
+    goalsOwn: totalGoalsOwn(o.teamHome as unknown as Match[]),
+    goalsBalance:
+      totalGoalsFavor(o.teamHome as unknown as Match[])
+      - totalGoalsOwn(o.teamHome as unknown as Match[]),
+    efficiency: efficiency(o.id, o.teamHome as unknown as Match[]),
+  }));
+
+export default calcLeaderBoardHome;
