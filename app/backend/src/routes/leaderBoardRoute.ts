0534b54a0a536e459a91f403449bc53da706f09d
Lucas <bithome1@outlook.com>
2022-12-16 19:32:58 -0300

    feat: Adicionado rota para o endpoint Get /leaderboard

--- app/backend/src/routes/leaderBoardRoute.ts
+++ app/backend/src/routes/leaderBoardRoute.ts
@@ -1,12 +1,15 @@
 import { Router } from 'express';
 import LeaderBoardHomeController from '../controllers/LeaderBoardHomeController';
 import LeaderBoardAwayController from '../controllers/LeaderBoardAwayController';
+import LeaderBoardController from '../controllers/leaderBoardController';
 
 const leaderBoardHomeController = new LeaderBoardHomeController();
 const leaderBoardAwayController = new LeaderBoardAwayController();
+const leaderBoardController = new LeaderBoardController();
 const route = Router();
 
 route.get('/home', leaderBoardHomeController.leaderBoardHome);
 route.get('/away', leaderBoardAwayController.leaderBoardAway);
+route.get('/', leaderBoardController.leaderBoard);
 
 export default route;
