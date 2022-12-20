719006f28f8f257325fbe541cde777f3e9922120
Lucas <bithome1@outlook.com>
2022-12-19 21:08:37 -0300

    feat: Criado  arquivo leaderBoardInterface.ts

---
+++ app/backend/src/interfaces/leaderBoardInterface.ts
@@ -0,0 +1,14 @@
+interface leaderboard {
+  name: string;
+  totalPoints: number;
+  totalGames: number;
+  totalVictories: number;
+  totalDraws: number;
+  totalLosses: number;
+  goalsFavor: number;
+  goalsOwn: number;
+  goalsBalance: number;
+  efficiency: string;
+}
+
+export default leaderboard;
